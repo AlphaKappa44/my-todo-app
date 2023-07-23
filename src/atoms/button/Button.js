0f71efa01305c588df4fd1e756e15ceef9ab3d02
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2023-07-23 23:48:02 +0200

    CSS to nearly zero

--- src/atoms/button/Button.js
+++ src/atoms/button/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 function Button() {
   return (
     <div className="button submit-button">
-      <button typeof='submit'></button>
+      <button typeof='submit'> VALIDER </button>
     </div>
   );
 }
--- src/atoms/title/Title.js
+++ src/atoms/title/Title.js
@@ -2,8 +2,8 @@ import './Title.css';
 
 function Title() {
   return (
-    <div className="Input">
-      <h1>Todo App</h1>1
+    <div className="Title">
+      <h1>Todo App</h1>
     </div>
   );
 }
