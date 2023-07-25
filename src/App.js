5b23904be16bec6411c3f156defe01cc4dfcb92b
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2023-07-25 23:32:23 +0200

    Added local storage on tasks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
+    // This function is a lazy initializer for the state, 
+    // meaning it will only be called once when the component is initially rendered.
+    const [tasks, setTasks] = useState(() => {
+        // getting stored value
+        const saved = localStorage.getItem("tasks");
+        const initialValue = JSON.parse(saved);
+        return initialValue || [];
+      });
+    const [newTask, setNewTask] = useState("");
 
-  const handleInputChange = (event) => {
-    setNewTask(event.target.value);
-  };
+    // Save tasks to localStorage whenever tasks state changes
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
-  const handleAddTask = () => {
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, newTask]);
-      setNewTask("");
-    }
-  };
+    const handleInputChange = (event) => {
+        setNewTask(event.target.value);
+    };
 
-  const handleDeleteTask = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
-  };
+    const handleAddTask = () => {
+        if (newTask.trim() !== "") {
+            setTasks([...tasks, newTask]);
+            setNewTask("");
+        }
+    };
 
-  return (
-    <div className="App">
-      <h1>Todo App</h1>
-      <div>
-        <input
-          type="text"
-          value={newTask}
-          onChange={handleInputChange}
-          placeholder="Enter a new task..."
-        />
-        <button onClick={handleAddTask}>Add Task</button>
-      </div>
-      <div >
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>
-              {task}{" "}
-              <button onClick={() => handleDeleteTask(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    const handleDeleteTask = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <div>
+                <input
+                    type="text"
+                    value={newTask}
+                    onChange={handleInputChange}
+                    placeholder="Enter a new task..."
+                />
+                <button onClick={handleAddTask}>Add Task</button>
+            </div>
+            <div >
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                            {task}{" "}
+                            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default App;
